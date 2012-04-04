621ccf8c2e24a305edda4596901b6cdccf5bb04d
Daniel Pereira <d.pereira@skillupjapan.co.jp>
2012-04-04 18:54:05 +0900

    Added graphs for morning and night side-by-side comparison

--- geoAnalyzer.R
+++ geoAnalyzer.R
@@ -1,46 +1,97 @@
 library(RMySQL)
 
-conn <- dbConnect(MySQL(), user="skillup", password="skillup", host="192.168.12.37", dbname="qori_analyzer")
-statement <- paste(
+#conn <- dbConnect(MySQL(), user="skillup", password="skillup", host="192.168.12.37", dbname="qori_analyzer")
+statementMorning <- paste(
 	"SELECT longitude, latitude",#timestamp, FROM_UNIXTIME(timestamp) AS date,
 	"FROM reports",
 	"WHERE uid LIKE '5eedd0514bbc4c2c7b77903f13dbf95f4693638f'",
-	"AND latitude > '-71'",
+	#"AND latitude > '-71'",
+	"AND EXTRACT(HOUR FROM timeofday) > 7",
+	"AND EXTRACT(HOUR FROM timeofday) < 10",
 	"group by longitude, latitude",
-	"ORDER BY timestamp ASC")#,
+	"ORDER BY timestamp ASC"),
+	#"LIMIT 100")
+statementNight <- paste(
+	"SELECT longitude, latitude",#timestamp, FROM_UNIXTIME(timestamp) AS date,
+	"FROM reports",
+	"WHERE uid LIKE '5eedd0514bbc4c2c7b77903f13dbf95f4693638f'",
+	#"AND latitude > '-71'",
+	"AND EXTRACT(HOUR FROM timeofday) > 18",
+	"AND EXTRACT(HOUR FROM timeofday) < 24",
+	"group by longitude, latitude",
+	"ORDER BY timestamp ASC"),
 	#"LIMIT 100")
 
-rso <- dbSendQuery(conn, statement)
-x1 <- fetch(rso)
+# Morning
+conn <- dbConnect(MySQL(), user="skillup", password="skillup", host="192.168.12.37", dbname="qori_analyzer")
+rsoM <- dbSendQuery(conn, statementMorning)
+xM <- fetch(rsoM)
+count <- nrow(xM)
 
 rowCount <- 0
-datax <- {}
-datay <- {}
-rso <- dbSendQuery(conn, statement)
-while(rowCount < 100) {
-		stopifnot(rowCount < 100)
-        y <- fetch(rso,n=1)
+dataxM <- {}
+datayM <- {}
+rsoM <- dbSendQuery(conn, statementMorning)
+while(rowCount < count) {
+		stopifnot(rowCount < count)
+        y <- fetch(rsoM,n=1)
         rowCount <- rowCount + nrow(y)
 
-        datax <- append(datax, y[1,1])
-        datay <- append(datay, y[1,2])
+        dataxM <- append(dataxM, y[1,1])
+        datayM <- append(datayM, y[1,2])
 }
-#print(data)
+
+# Night
+conn <- dbConnect(MySQL(), user="skillup", password="skillup", host="192.168.12.37", dbname="qori_analyzer")
+rsoN <- dbSendQuery(conn, statementNight)
+xN <- fetch(rsoN)
+count <- nrow(xN)
+
+rowCount <- 0
+dataxN <- {}
+datayN <- {}
+rsoN <- dbSendQuery(conn, statementNight)
+while(rowCount < count) {
+		stopifnot(rowCount < count)
+        y <- fetch(rsoN,n=1)
+        rowCount <- rowCount + nrow(y)
+
+        dataxN <- append(dataxN, y[1,1])
+        datayN <- append(datayN, y[1,2])
+}
+
+###
+# Graphs
+###
 
 # Scatterplots with smoothed densities color representation
 require("geneplotter")
 require("RColorBrewer")
-x <- rbind(x1)
-layout(matrix(1:4, ncol=2, byrow=TRUE))
+require("hdrcde")
+
+layout(matrix(1:6, ncol=2, byrow=FALSE))
 op <- par(mar=rep(2,4))
-smoothScatter(x, nrpoints=0)
-smoothScatter(x)
-#smoothScatter(x, nrpoints=Inf, colramp=colorRampPalette(brewer.pal(9,"YlOrRd")), bandwidth=40)
-colors <- densCols(x)
-plot(x, col=colors, pch=20)
+
+# Morning
+x1 <- rbind(xM)
+#smoothScatter(x1, nrpoints=0)
+smoothScatter(x1)
+#smoothScatter(x1, nrpoints=Inf, colramp=colorRampPalette(brewer.pal(9,"YlOrRd")), bandwidth=40)
+colors <- densCols(x1)
+plot(x1, col=colors, pch=20)
 
 # Bivariate Highest density regions
-require("hdrcde")
-#par(mfrow=c(1,2))
 #plot(datax,datay, pch="+", cex=.5)
-hdr.boxplot.2d(datax,datay)
+hdr.boxplot.2d(dataxM,datayM)
+
+# Night
+x2 <- rbind(xN)
+#smoothScatter(x2, nrpoints=0)
+smoothScatter(x2)
+#smoothScatter(x2, nrpoints=Inf, colramp=colorRampPalette(brewer.pal(9,"YlOrRd")), bandwidth=40)
+colors <- densCols(x2)
+plot(x2, col=colors, pch=20)
+
+# Bivariate Highest density regions
+#plot(datax,datay, pch="+", cex=.5)
+hdr.boxplot.2d(dataxN,datayN)
